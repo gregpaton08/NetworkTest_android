3a78eed583b71b25c79830cc78db2be70be91bf9
gregpaton08 <gregpaton08@gmail.com>
2014-02-03 01:05:37 -0500

    [add] Added background task for network communication.

--- NetworkTest/src/main/java/com/example/networktest/MainActivity.java
+++ NetworkTest/src/main/java/com/example/networktest/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +17,14 @@ import android.view.ViewGroup;
 import android.os.Build;
 import android.view.Window;
 import android.content.pm.ActivityInfo;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends Activity {
 
@@ -36,6 +45,54 @@ public class MainActivity extends Activity {
         }
     }
 
+    private class BackgroundTask extends AsyncTask<URL, Integer, String> {
+        protected String doInBackground(URL... urls) {
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                try {
+                    HttpURLConnection urlConnection = (HttpURLConnection) urls[0].openConnection();
+
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setChunkedStreamingMode(0);
+
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+                    byte[] sendStr = new byte[2];
+                    sendStr[0] = '4';
+                    sendStr[1] = 0;
+                    out.write(sendStr);
+                    byte[] buf = new byte[64];
+                    in.read(buf);
+                    Log.w("NetworkTest", String.format("%s", buf));
+                    return String.format("%s", buf);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.w("NetworkTest", "Network unavailable");
+            }
+
+            return "Fail";
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected  void onPreExecute() {
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.w("NetworkTest", String.format("%s", result));
+        }
+    }
+
     // Callback for Send button
     public void onClickSend(View view) {
         Log.w("NetworkText", "Send");
@@ -44,7 +101,29 @@ public class MainActivity extends Activity {
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
-            // fetch data
+            try {
+                URL url = new URL("http://192.168.0.31:12345");
+
+                new BackgroundTask().execute(url);
+//                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//
+//                urlConnection.setDoOutput(true);
+//                urlConnection.setChunkedStreamingMode(0);
+
+//                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//                OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+//
+//                byte[] sendStr = new byte[2];
+//                sendStr[0] = '4';
+//                sendStr[1] = 0;
+//                //out.write(sendStr);
+//                byte[] buf = new byte[64];
+//                //in.read(buf);
+//                //Log.w("NetworkTest", String.format("%s", buf));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             Log.w("NetworkTest", "Network unavailable");
         }
